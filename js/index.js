3ae315ada8b8ea78650445923416496d632d7d24
blurleisuree <ipishir@gmail.com>
2023-02-21 23:05:51 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    // carousel -------------------------------------------------------------
+
+    const carouselInner = $('.carousel-inner');
+
+    let slidesArr = ['img/slide1.jpg', 'img/slide2.jpg', 'img/slide3.jpg',];
+
+    $.each(slidesArr, function (index, elem) {
+        $(carouselInner).append("<div class='carousel-item' data-bs-interval='3000'></div>");
+        let itemNow = $(".carousel-item");
+        itemNow = itemNow[itemNow.length - 1];
+
+        if (index == 0) {
+            $(itemNow).addClass("active");
+        };
+        $(itemNow).append(`<img class='d-block w-100' alt='slide${index + 1}' src='${elem}' </img>`);
+    })
+
+
+});
